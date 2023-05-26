84ec8147dbbdbef7913dba946cf9aff2036ba71b
颜曾一 <simonyen@foxmail.com>
2023-05-26 16:02:21 +0800

    还是有点搞不清哦，云里雾里，没事坚持下去就好了

---
+++ lock/sem.hpp
@@ -0,0 +1,48 @@
+#ifndef SEM_HPP
+#define SEM_HPP
+
+#include <exception>
+#include <semaphore.h>
+
+/*
+该文件实现了对信号量的封装
+*/
+
+class Sem
+{
+private:
+	sem_t sem;
+
+public:
+	// 构造函数，初始化信号量
+	Sem()
+	{
+		/*
+			sem_init:
+				第二个参数为0代表该信号量是当前进程的局部信号量，
+				否则会多进程之间共享这个信号量。
+
+				第三个参数代表信号量的初始值。
+		*/
+		if (sem_init(&sem, 0, 0) != 0)
+			throw std::exception();
+	}
+	// 销毁信号量
+	~Sem()
+	{
+		sem_destroy(&sem);
+	}
+	// 等待信号量
+	bool wait()
+	{
+		// 以原子操作的方式将信号量减一，如果信号量为0则会阻塞
+		return sem_wait(&sem) == 0;
+	}
+	// 增加信号量
+	bool post()
+	{
+		return sem_post(&sem) == 0;
+	}
+};
+
+#endif
